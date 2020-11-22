d57000516e73680b0063dafc77a468c9097a5f60
EJostad <eric.jostad@outlook.com>
2020-11-21 21:28:07 -0800

    Added link to deployed sit along with screenshot to readme. Updated generatedPassword function to iniclude variables and if statements, along with 1 for loop.

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ Please see the link to the site below along with a correlating screenshot:
 
 https://ejostad.github.io/Password-Generator/
 
+![Password Generator](https://user-images.githubusercontent.com/71619046/99895407-114fc600-2c3d-11eb-8f94-ee84967153da.png)
--- script.js
+++ script.js
@@ -17,12 +17,12 @@ function writePassword() {
 
 }
 
-function generatePassword(){
+  function generatePassword(){
   var passwordLength = parseInt(prompt("Please specify password length between 8 and 128 characters."));
-    if( passwordLength === ""){
+    if( passwordLength == ""){ 
       alert("Length must be specified.");
       return ("Please try again.");
-    }   else{
+    }
     if(passwordLength < 8){
       alert("Password must be greater than 8 characters.");
       return ("Please try again.");
@@ -31,7 +31,6 @@ function generatePassword(){
       alert("Password must be less than 128 characters.");
       return ("Please try again.");
     }
-  }
 
   var password = "";
   var possibleChar = "";
@@ -39,29 +38,31 @@ function generatePassword(){
   var includeuppCase = confirm("Should uppercase letters be included?");
     if(includeuppCase){ 
       possibleChar = possibleChar + uppCase;
-    }
+    } 
 
   var includelowCase = confirm("Should lowercase letters be included?");
     if(includelowCase){ 
     possibleChar = possibleChar + lowCase;
-    }
+    } 
+
   var includenum = confirm("Should numbers be included?");
   if(includenum){ 
     possibleChar = possibleChar + num;
-    }
+    } 
+
   var includespecChar = confirm("Should special characters be included?");
   if(includespecChar){ 
     possibleChar = possibleChar + specChar;
     }
 
-  for (var i = 0; i < passwordLength; i++){
+  for(var i = 0; i < passwordLength; i++){
     var random = Math.floor(Math.random() * possibleChar.length);
     password = password + possibleChar[random];
   } 
     
   return password;
 
-}
+  }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
